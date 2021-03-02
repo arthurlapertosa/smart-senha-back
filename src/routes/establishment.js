13ba5d2f27665c7495d2320f6450b33b8ff79b71
Arthur de Lapertosa Lisboa <alisboa@google.com>
2021-03-01 22:09:27 -0300

    Add route and method to return all establishments within the user's radius

--- src/routes/establishment.js
+++ src/routes/establishment.js
@@ -2,6 +2,7 @@ const router = require("express-promise-router")();
 const establishmentController = require("../controllers/establishmentController");
 
 router.get('/', establishmentController.getEstablishments);
+router.get('/inRadius', establishmentController.getAllInRadius);
 router.post('/:id', establishmentController.isOnRadius);
 
 module.exports = router;
