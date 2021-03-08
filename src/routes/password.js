fa4fd087f7b0faadef40e98708c0eba2da891d6d
João Victor <joaovictor_bl@hotmail.com>
2021-03-08 20:21:11 -0300

    delete

--- src/routes/password.js
+++ src/routes/password.js
@@ -1,9 +1,14 @@
 const router = require("express-promise-router")();
 const passwordController = require("../controllers/passwordController");
-const {auth} = require("../controllers/middleware/auth");
+const { auth } = require("../controllers/middleware/auth");
 
 router.post("/", auth, passwordController.createPassword);
 router.get("/", auth, passwordController.getUserPassword);
 router.get("/all", passwordController.getPassword);
+router.get(
+  "/byEstablishment/:id",
+  passwordController.getPasswordsByEstablishment
+);
+router.delete("/:id", passwordController.deletePassword);
 
 module.exports = router;
